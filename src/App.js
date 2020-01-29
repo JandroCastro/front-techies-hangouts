7a85ee399492390d688607bae97317127aba8bcb
Alejandro Castro Alarcón <jandroportonovo@hotmail.com>
2020-01-29 12:15:05 +0100

    rutas en la página principal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { AuthProvider } from "./context/auth-context";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <AuthProvider>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/home">
+              <Dashboard />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/hangout">
+              <DetailedHangout />
+            </Route>
+          </Switch>
+        </AuthProvider>
+      </BrowserRouter>
+      );
+    </div>
+  );
 }
 
 export default App;
