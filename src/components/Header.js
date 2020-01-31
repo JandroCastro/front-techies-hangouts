745ec57134a8d3ad7617449451af96daf9941ef7
Alejandro Castro Alarcón <jandroportonovo@hotmail.com>
2020-01-31 11:33:27 +0100

    copiar esquema de css y conectar header con home

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export function Header() {
+  return (
+    <React.Fragment>
+      <header>
+        <div>
+          <p>Icono</p>
+        </div>
+        <div>
+          <h1>TITULO</h1>
+        </div>
+        <div>
+          <p>Hola Chaval</p>{" "}
+        </div>
+      </header>
+    </React.Fragment>
+  );
+}
