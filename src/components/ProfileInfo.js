1a47f8b817a244333aa1d1a53399be0b6ef51ba5
JoseMagdalena <jose_sera_sera@hotmail.com>
2020-02-05 12:20:09 +0100

    página de tu perfil, meter logos maquetar con flex

--- src/components/ProfileInfo.js
+++ src/components/ProfileInfo.js
@@ -1,10 +1,14 @@
 import React from "react";
+import logoLinkedin from "../img/linkedin.svg"
+import logoFacebook from "../img/facebook.svg"
+import logoTwitter from "../img/twitter.svg"
+import logoInstagram from "../img/instagram.svg"
 
 export function ProfileInfo() {
   return (
     <React.Fragment>
       <div>
-        <ul>
+        <ul className="infoperfil">
           <li>Category</li>
           <li>Edad</li>
           <li>Position</li>
@@ -12,7 +16,23 @@ export function ProfileInfo() {
         </ul>
       </div>
       <div id="enlacesRRSS">
-        <a href="/">Enlaces a GitHub, Linkedin, Instagram, Blogs..</a>
+      <li> <a  href="https://www.instagram.com/">
+        <img src={logoInstagram} alt="logo insta"/>
+        </a></li>
+        <li>
+          <a href="https://www.linkedin.com/">
+            <img src={logoLinkedin}alt="logo linkedin" />
+          </a>
+          </li>
+          <li>
+      <a href="https://www.facebook.com/">
+       <img src={logoFacebook} alt="logo facebook"/>
+       </a>
+      </li>
+      <li><a href="https://www.twitter.com/">
+        <img src={logoTwitter} alt="logo twitter"/>
+        </a></li>
+        
       </div>
     </React.Fragment>
   );
