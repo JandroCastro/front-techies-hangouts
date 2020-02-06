e8b67dafdf98d95b500cb1b14c41749ac4ca2a5c
SERVICRO <servicro@gmail.com>
2020-02-06 19:35:26 +0100

    creado yourhangout en js y css y el nuevo componente small-cards

--- src/components/SmallCards.js
+++ src/components/SmallCards.js
@@ -10,14 +10,14 @@ const calc = (x, y) => [
 const trans = (x, y, s) =>
   `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
-export function HangoutCards() {
+export function SmallCards() {
   const [props, set] = useSpring(() => ({
     xys: [0, 0, 1],
     config: { mass: 2.5, tension: 100, friction: 50 }
   }));
   return (
     <animated.div
-      class="card"
+      class="smallCard"
       onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
       onMouseLeave={() => set({ xys: [0, 0, 1] })}
       style={{ transform: props.xys.interpolate(trans) }}
