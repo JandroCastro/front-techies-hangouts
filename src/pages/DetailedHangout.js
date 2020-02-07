e12ded7f12422a86aef0b9a7ebbd95dc1ff678ab
SERVICRO <servicro@gmail.com>
2020-02-07 20:16:11 +0100

    refactoring de detailedHangout js y css

--- src/pages/DetailedHangout.js
+++ src/pages/DetailedHangout.js
@@ -30,47 +30,53 @@ export function DetailedHangout(id) {
   return (
     <React.Fragment>
       <Header title="The hangout" />
-      <section id="principalDeQuedada">
+      <main className="detailedHangout">
         <h1 style={{ marginTop: 12 }}>Cañas y copas afterwork</h1>
-        <ul>
+        <ul id="portada">
           <li id="fotoDeQuedada"></li>
           <li id="avatar">
             <AvatarContainer />
           </li>
+          <li>
+            <button id="anotarse">Anotarse</button>
+          </li>
         </ul>
-        <button id="anotarse">Anotarse</button>
-      </section>
-      <section id="info">
-        <div id="datosQuedada">
-          <ul>
-            <h3>Detalles</h3>
-            <li>Ciudad</li>
-            <li>Fecha</li>
-            <li>Hora</li>
-            <li>Temática</li>
-            <li>Capacidad</li>
-            <li>Descripción</li>
-            <li>Mapa</li>
-            <li>
-              <button id="editar">editar quedada</button>
-            </li>
-          </ul>
-        </div>
-        <div id="detallesAsistentes">
-          <ul id="confirmados">
-            <h3>Confirmados</h3>
-            <li>Pepe</li>
-            <li>Juan</li>
-            <li>Manolo</li>
-          </ul>
-          <ul id="pendientes">
-            <h3>Pendientes</h3>
-            <li>Rosaura</li>
-            <li>Rosalinda</li>
-            <li>RosaMari</li>
-          </ul>
-        </div>
-      </section>
+
+        <section id="info">
+          <div id="datosQuedada">
+            <ul>
+              <h3>Detalles</h3>
+              <li>Ciudad</li>
+              <li>Fecha</li>
+              <li>Hora</li>
+              <li>Temática</li>
+              <li>Capacidad</li>
+              <li>Descripción</li>
+              <li>Mapa</li>
+              <li>
+                <button id="editar">editar quedada</button>
+              </li>
+            </ul>
+          </div>
+          <div id="detallesAsistentes">
+            <ul id="confirmados">
+              <h3>Confirmados</h3>
+              <li>
+                <miniAvatar />
+                Pepe
+              </li>
+              <li>Juan</li>
+              <li>Manolo</li>
+            </ul>
+            <ul id="pendientes">
+              <h3>Pendientes</h3>
+              <li>Rosaura</li>
+              <li>Rosalinda</li>
+              <li>RosaMari</li>
+            </ul>
+          </div>
+        </section>
+      </main>
       <Footer />
     </React.Fragment>
   );
