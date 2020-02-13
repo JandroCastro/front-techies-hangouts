30ea873ed186b78125e6b193b9aa99ce7461a695
Alejandro Castro Alarcón <jandroportonovo@hotmail.com>
2020-02-13 17:25:00 +0100

    acabar de importar los datos para mostrar en las cards

--- src/components/HangoutCards.js
+++ src/components/HangoutCards.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { useSpring, animated } from "react-spring";
+import { getThematicName, getCityName } from "../http/utilitiesService";
 
 const calc = (x, y) => [
   -(y - window.innerHeight / 2) / 10,
@@ -9,10 +10,23 @@ const calc = (x, y) => [
 ];
 const trans = (x, y, s) => `scale(${s})`;
 
-export function HangoutCards({ prop }) {
-  console.log(prop);
-  const imgUrl =
-    "https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80";
+export function HangoutCards({ event }) {
+  const [city, setCity] = useState("");
+  const [thematic, setThematic] = useState("");
+
+  const date = event.event_date.split("T");
+  const hour = event.event_hour.substring(0, 5);
+
+  useEffect(() => {
+    getCityName(event.city_id).then(response => {
+      setCity(response.data[0].name);
+    });
+    getThematicName(event.thematic_id).then(response => {
+      setThematic(response.data[0].name);
+    });
+  }, []);
+
+  console.log(thematic, city);
 
   const [props, set] = useSpring(() => ({
     xys: [0, 0, 1],
@@ -24,12 +38,21 @@ export function HangoutCards({ prop }) {
       onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
       onMouseLeave={() => set({ xys: [0, 0, 1] })}
       style={{
-        transform: props.xys.interpolate(trans),
-        backgroundImage: "url(" + prop.photo_url + ")"
+        transform: props.xys.interpolate(trans)
       }}
     >
-      <div id="hangout-title">
-        <h3>{prop.title}</h3>
+      <div
+        id="hangout-img"
+        style={{ backgroundImage: "url(" + event.photo_url + ")" }}
+      ></div>
+      <div id="hangout-info">
+        <h3>{event.title}</h3>
+        <div>
+          <h5>{city}</h5>
+          <h5>{date[0]}</h5>
+          <h5>{hour}</h5>
+          <h5>{thematic}</h5>
+        </div>
       </div>
     </animated.div>
   );
