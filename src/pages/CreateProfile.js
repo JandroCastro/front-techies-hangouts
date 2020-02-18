91437c971d3a9e6143421f7717f7029d1221cc47
Alejandro Castro Alarcón <jandroportonovo@hotmail.com>
2020-02-18 11:53:27 +0100

    descargar datos de Backend para precargar formularios

--- src/pages/CreateProfile.js
+++ src/pages/CreateProfile.js
@@ -1,23 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { Footer } from "../components/Footer";
-import { getProfile, getAvatar } from "../http/profileService";
-import { Header } from "../components/Header"
+import {
+  getProfile,
+  getAvatar,
+  updateProfile,
+  updateAvatar
+} from "../http/profileService";
+import { Header } from "../components/Header";
+import { useHistory } from "react-router-dom";
 
 export function CreateProfile() {
+  const storedUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  const userId = storedUser.id;
+
+  const history = useHistory();
+
   const [profile, setProfile] = useState({});
   const [avatar, setAvatar] = useState("");
 
   useEffect(() => {
-    getProfile().then(response => setProfile(response.data));
-  }, [profile]);
+    getProfile(userId).then(response => setProfile(response.data[0]));
+    getAvatar(userId).then(response => setAvatar(response.data[0]));
+  }, []);
 
-  useEffect(() => {
-    getAvatar().then(response => setAvatar(response.data));
-  }, [avatar]);
+  const handleSubmit = formData => {
+    updateProfile(userId, formData)
+      .then(history.push(`/profile/${userId}`))
+      .catch();
+  };
 
   return (
     <React.Fragment>
-     <Header title="CREA TU PERFIL"/>
+      <Header title="CREA TU PERFIL" />
       <div>
         <form className="hangout" action="#">
           <label className="label">Nombre y apellidos</label>
@@ -34,11 +49,7 @@ export function CreateProfile() {
             type="text"
             placeholder="introduce una breve descripción sobre ti"
           />
-          <button
-            onClick={() => (window.location.href = "/profile")}
-            className="btn"
-            type="button"
-          >
+          <button onClick={handleSubmit} className="btn" type="submit">
             CREAR
           </button>
         </form>
